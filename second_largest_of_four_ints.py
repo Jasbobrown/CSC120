d72862f60fd7828c218afa6cdd385db4135da854
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-07 16:09:35 -0400

    Update second_largest_of_four_ints.py

--- second_largest_of_four_ints.py
+++ second_largest_of_four_ints.py
@@ -1,9 +1,2 @@
 L = [int(input()), int(input()), int(input()), int(input())]
-max, sec = L[0], L[0]
-for i in L:
-    if i > max:
-        max = i
-for i in L:
-    if i > sec and i < max:
-        sec = i
-print(sec)
+print(sorted(L)[-2])
