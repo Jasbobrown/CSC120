9dce1bab2a1dc0b100f52bcb81aa5d08b5d84600
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-07 19:40:15 +0000

    update

--- second_largest_of_four_ints.py
+++ second_largest_of_four_ints.py
@@ -1,2 +1,9 @@
-a, b, c, d = int(input()), int(input()), int(input()), int(input())
-print(max([a,b,c,d].remove(max([a,b,c,d]))))
+L = [int(input()), int(input()), int(input()), int(input())]
+max, sec = L[0], L[0]
+for i in L:
+    if i > max:
+        max = i
+for i in L:
+    if i > sec and i < max:
+        sec = i
+print(sec)
