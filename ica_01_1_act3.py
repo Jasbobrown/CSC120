4114777ae7b8ffa1cad2c3cee25bfa1b865ac376
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-07 23:40:23 +0000

    update

--- ica_01_1_act3.py
+++ ica_01_1_act3.py
@@ -1,5 +1,5 @@
-print("The function str_mult() has been called.  For this activity, we won't print out any details about its parameters.")
 def str_mult(a, b):
+    print("The function str_mult() has been called.  For this activity, we won't print out any details about its parameters.")
     out = ''
     for _ in range(b):
         out += a
