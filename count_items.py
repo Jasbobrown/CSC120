cce4313b446aaad64b853d94aec17bc387914419
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-11 20:12:27 +0000

    update

--- count_items.py
+++ count_items.py
@@ -6,16 +6,25 @@ def main():
     for line in file.readlines():
         if not line or not line.strip() or line.lstrip()[0] == '#':
             continue
-        
-        print(line.split())
         if line.split()[0] in D:
             D[line.split()[0]] += int(line.split()[1])
         else:
             D[line.split()[0]] = int(line.split()[1])
-        
-    for k,v in D.items():
-        L.append((v, k))
     
-    for tup in sorted(L):
-        print(tup[0], tup[1])
-        
+    print("STEP 1: THE ORIGINAL DICTIONARY")
+    for k in sorted(D.keys()):
+        print(f"Key: {k} Value: {D[k]}")
+
+    print("STEP 2: A LIST OF VALUE->KEY TUPLES")
+    for k in sorted(D.keys()):
+        L.append((D[k], k))
+        print(L)
+    
+    print("STEP 3: AFTER SORTING")
+    print(sorted(L))
+
+    print("STEP 4: THE ACTUAL OUTPUT")
+    for i in L:
+        print(f"{i[0]} {i[1]}")
+
+main()
