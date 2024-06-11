6f7eba79e9f81eac1079d7c82e4414cd9febc0f1
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-11 14:13:46 -0400

    update

---
+++ swap.py
@@ -0,0 +1,9 @@
+def main():
+    x = input("Please give a string to swap:")
+    x = x.lstrip().rstrip()
+    if len(x) % 2 == 0:
+        print(x[len(x)/2:] + x[:len(x)/2])
+    print(x[(len(x)//2) + 2:] + x[(len(x)//2) + 1] + x[:len(x)//2])
+
+main()
+
