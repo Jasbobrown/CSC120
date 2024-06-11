6090219f19c6fb7aac4d7c770127727649ad652a
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-11 19:47:48 +0000

    update

--- population.py
+++ population.py
@@ -2,26 +2,34 @@ def main():
     x = input("file: ")
     x = x.lstrip().rstrip()
     file = open(x)
-    states, population = [], 0
+    states, population, multiWord = [], 0, 0
     for line in file.readlines():
         if not line or not line.strip() or line.lstrip()[0] == '#':
             continue
-        if len(line.split(' ', 1)) > 1:
-            state = line.split(' ', 1)[0] + ' ' + line.split(' ', 1)[1].split()[0]
-            pop = line.split(' ', 1)[1].split()[1]
+
+        for i in range(1, len(line) - 1):
+            if line[i] == ' ' and line[i-1].isalpha() and line[i+1].isalpha():
+                multiWord += 1 
+
+        if multiWord:
+            state = " ".join(line.split(' ')[:multiWord + 1])
+            pop = line.split()[-1]
             states.append(state)
             population += int(pop)
             print(f"State/Territory: {state}")
             print(f"Population: {int(pop)}")
             print()
+            multiWord = 0
+
         else:
             state = line.split()[0]
             pop = line.split()[1]
             states.append(state)
-            pops += int(pop)
+            population += int(pop)
             print(f"State/Territory: {state}")
             print(f"Population: {int(pop)}")
             print()
+            
     print(f"# of States/Territories: {len(states)}")
     print(f"Total Population: {population}")
 
