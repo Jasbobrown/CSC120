ec96c288bbf198391f70457eb3dc009908d2e94b
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-11 18:31:27 +0000

    update

--- swap.py
+++ swap.py
@@ -2,7 +2,8 @@ def main():
     x = input("Please give a string to swap:")
     x = x.lstrip().rstrip()
     if len(x) % 2 == 0:
-        print(x[len(x)/2:] + x[:len(x)/2])
-    print(x[(len(x)//2) + 2:] + x[(len(x)//2) + 1] + x[:len(x)//2])
+        print(x[int(len(x)/2):] + x[:int(len(x)/2)])
+    else:
+        print(x[(len(x)//2) + 1:] + x[(len(x)//2)] + x[:(len(x)//2)])
 main()
 
