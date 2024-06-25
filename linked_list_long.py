c69db1638ba9f76c4f95dbe7bc830f0ae14011bf
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-06-25 00:27:52 +0000

    up

--- linked_list_long.py
+++ linked_list_long.py
@@ -5,13 +5,24 @@ def is_sorted(head):
     if not cur or not cur.next:
         return True
     while cur.next != None:
-        if cur.next < cur:
+        if cur.next.val < cur.val:
             return False
+        cur = cur.next
     return True
     
 
-def list_sum():
-    pass
+def list_sum(head):
+    cur = head
+    sum = 0
+    if not cur:
+        return sum
+    if not cur.next:
+        return sum + cur.val
+    while cur.next != None:
+        sum += cur.val
+        cur = cur.next
+    return sum
+
 
 def partition_list():
     pass
