fb3eb7522610bb64887e3197de8a03ca1542bc6f
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-07-09 18:38:58 +0000

    update

---
+++ annoying_recursion_long.py
@@ -0,0 +1,13 @@
+def annoying_fibonacci_sequence(n):
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
+    if n == 2:
+        return [0, 1]
+    out = annoying_fibonacci_sequence(n-1)
+    out.append(out[-1] + out[-2])
+    return out
+
+
+print(annoying_fibonacci_sequence(6))
