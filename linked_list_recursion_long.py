5ece39844b026b75d53da11336daa24b1326138a
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-07-09 19:44:34 +0000

    update

--- linked_list_recursion_long.py
+++ linked_list_recursion_long.py
@@ -1,21 +1,4 @@
-class ListNode:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-    def __str__(self):
-        vals = []
-        objs = set()
-        curr = self
-        while curr is not None:
-            curr_str = str(curr.val)
-            if curr in objs:
-                vals.append("{} -> ... (to infinity and beyond)".format(curr_str))
-                break
-            else:
-                vals.append(curr_str)
-                objs.add(curr)
-            curr = curr.next
-        return " -> ".join(vals)
+from list_node import ListNode
 
 def array_to_list_recursive(data):
     if not data:
