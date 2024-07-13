ec9ab00351fca99e609e8979567a25ffe31a7e71
Jasbobrown <99360460+Jasbobrown@users.noreply.github.com>
2024-07-13 23:44:00 +0000

    update

--- tree_funcs_long.py
+++ tree_funcs_long.py
@@ -1,4 +1,37 @@
 from list_node import ListNode
 
 def bst_search_loop(root, val):
+    cur = root
+    while cur:
+        if cur.val == val:
+            return cur
+        elif val < cur.val:
+            cur = cur.left
+        else:
+            cur = cur.right
+    return None
+
+def tree_search(root, val):
     pass
+
+def bst_insert_loop(root, val):
+    pass
+
+def pre_order_traversal_print(root):
+    pass
+
+def in_order_traversal_print(root):
+    pass
+
+def post_order_traversal_print(root):
+    pass
+
+def in_order_vals(root):
+    pass
+
+def bst_max(root):
+    pass
+
+def tree_max(root):
+    pass
+
